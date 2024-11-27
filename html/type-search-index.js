bcb52f2ea642ad851a0b65029a91a7a9dcff45bd
Matthew-PV <matthew.puentevillegasmichavil@usp.ceu.es>
2024-11-27 20:18:31 +0100

    Eliminación Archivos Antiguos

---
+++ html/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dominio","l":"Ansi"},{"p":"interfaz","l":"Interfaz"}]
--- src/aplicacion/Principal.java
+++
@@ -1,9 +0,0 @@
-package aplicacion;
-import interfaz.Interfaz;
-
-public class Principal {
-    public static void main(String[] args) {
-        Interfaz interfaz = new Interfaz();
-        interfaz.ejecutar();
-    }
-}
