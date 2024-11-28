828d91487ad609ed08b76911c7346fe09930ddde
Matthew-PV <matthew.puentevillegasmichavil@usp.ceu.es>
2024-11-28 23:15:11 +0100

    v3 Creación Infraestructura

---
+++ html/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"aplicacion"},{"l":"control"},{"l":"dominio"},{"l":"dominio.productos"},{"l":"interfaz"}];updateSearchResults();
