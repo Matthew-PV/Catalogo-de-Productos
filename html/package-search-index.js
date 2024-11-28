4c23b312c7ee2327183c22f163447d26a2f3811c
Matthew-PV <matthew.puentevillegasmichavil@usp.ceu.es>
2024-11-28 09:33:19 +0100

    v2.1

--- html/package-search-index.js
+++ html/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"aplicacion"},{"l":"control"},{"l":"dominio"},{"l":"interfaz"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"aplicacion"},{"l":"control"},{"l":"dominio"},{"l":"interfaz"}]
